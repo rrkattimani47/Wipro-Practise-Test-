39a7f534633c0c5761e681eb023c897995421017
Rashmi Kattimani <rrkattimani47@gmail.com>
2020-08-07 16:38:08 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,24 @@
+Using Java 7
+Link to the test: https://tests.mettl.com/authenticateKey/8c1f2ae
+
+import java.io.*;
+import  java.util.*;
+
+//Read only user code
+class UserManiCode
+{
+public int nFactorial(int input1){
+//Read only region ends here
+//Write the code here....
+   
+   
+   int fact=1;
+		for(int i=input1;i>0;i--)
+		{
+			fact=fact*i;
+		}
+			return fact;
+	
+	}
+
+}
